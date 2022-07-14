40a2dc8cfeb34bc645757ad4bcab19fced6c1f9b
Mustafa-ilahi <mustafailahi586@gmail.com>
2022-07-14 14:04:10 -0800

    getting started

---
+++ src/services/utilities/colors/index.js
@@ -0,0 +1,17 @@
+export const colors = {
+    white: '#ffffff',
+    black: '#000000',
+    error: 'rgba(200, 0, 0, 0.8)',
+    gray: '#666',
+    appColor1: '#1D3932',
+    appTextColor1: '#000000',
+    appTextColor2: 'rgba(26,26,26, 1)',
+    appTextColor3: 'rgba(49,49,49, 1)',
+    appTextColor4: '#A1A1A1',
+    appTextColor5: '#FFFF',
+    appBgColorGradiant: ['#27AE8C', '#1D3932'],
+    appBgColor1: '#FFFF',
+    appBgColor2: '#E6E6E6',
+    appBgColorPrimary: '#1D3932',
+    appBgColor3: '#23a455',
+  };
---
+++ src/services/utilities/images/index.js
@@ -0,0 +1,2 @@
+import signUpBg from '../../../assets/bg.png';
+import loginBg from '../../../assets/bg2.png';
